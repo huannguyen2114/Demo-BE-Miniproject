c4c38de24b13ae6b06fc30dd48b07b313189f553
huannguyen2114 <huannguyen2114@huannguyen2114s-Laptop.local>
2024-06-10 15:45:56 +0700

    fix bug

--- Controllers/FoodControler/createFoodAPI.js
+++ Controllers/FoodControler/createFoodAPI.js
@@ -1,19 +1,18 @@
 import { models } from "../../Models/index.js";
 
-
 export async function createFood(req, res) {
-    const {categoryId,name,quantity,price} = req.body;
+    const { categoryId, name, quantity, price } = req.body;
     console.log(categoryId, name, quantity, price);
     try {
         const newFood = await models.Food.create({
-            categoryId: 50000,
-            name : name,
+            categoryId: categoryId,
+            name: name,
             quantity: quantity,
-            price: price
-        })
+            price: price,
+        });
         res.status(200).json(newFood);
-    } catch (err){
+    } catch (err) {
         console.log(err);
-        res.status(500).json({ "message": "Failed server food logic" })
+        res.status(500).json({ message: "Failed server food logic" });
     }
 }
